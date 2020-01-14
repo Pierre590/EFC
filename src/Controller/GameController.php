ab5de1508658223c1fc0bc93b4f072dbe45abf62
pierre <nanashi4781@gmail.com>
2020-01-14 14:17:29 +0100

    okavantpagination

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -5,20 +5,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Game;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
 class GameController extends AbstractController
 {
     /**
-     * @Route("/", name="game")
+     * @Route("/", name="games")
      */
     public function index()
     {
         $games = $this->getDoctrine()
         ->getRepository(Game::class)
-        ->findAll();
+        ->findBY([],['name'=>'ASC']);
 
         return $this->render('game/index.html.twig', [
             'games' => $games,
+        ]);
+    }
+    /**
+     * @Route("/game/{id}", name="game")
+     */
+    public function game($id)
+    {
+        $game = $this->getDoctrine()
+        ->getRepository(Game::class)
+        ->find($id);
+
+
+
+        return $this->render('game/game.html.twig', [
+            'game' => $game,
         ]);
     }
 
